c00a04d50231d676ce75e7c580ac0d3c60eb0a6b
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2020-09-03 15:17:41 +0900

    속성 안맞음

--- TeamProject/config.py
+++
@@ -1,9 +0,0 @@
-import os
-SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost:3306/flask_teamproject'
-SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-JSON_AS_ASCII = False
-SECRET_KEY = "123456789"
-
-# UPLOAD_FOLDER = 'C:/Users/win7/Documents/GitHub/WEB-Project1/TeamProject/static/img'
-# ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
