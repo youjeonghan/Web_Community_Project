09cfffb59a1496e6ba3c94f340ee3cfbb935f5dd
Jerry <shin101004@naver.com>
2020-10-28 18:44:03 +0900

    commit
    js test

---
+++ TeamProject/static/js/config.js
@@ -0,0 +1,13 @@
+export const POST= 'http://127.0.0.1:5000/api/post';
+export const FILE_UPLOAD= 'http://127.0.0.1:5000/api/postupload';
+export const USER_INFO= 'http://127.0.0.1:5000/api/user_info';
+export const BOARD= 'http://127.0.0.1:5000/api/board_info';
+export const COMMENT = 'http://127.0.0.1:5000/api/comment/';
+export const POSTLIKES = 'http://127.0.0.1:5000/api/postlike/';
+export const COMMENTLIKES = 'http://127.0.0.1:5000/api/commentlike/';
+export const BEST_POST = 'http://127.0.0.1:5000/api/bestpost';
+export const USER_SPECIFIC = 'http://127.0.0.1:5000/api/user_specific_info/';
+export const SEARCH = 'http://127.0.0.1:5000/api/search';
+export const REPORT = 'http://127.0.0.1:5000/api/report_post/';
+export const REPORT_COMMENT = 'http://127.0.0.1:5000/api/report_comment/';
+export const CHECK_AUTH = 'http://127.0.0.1:5000/api//who_are_you';
