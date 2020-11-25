9c3681fbe398849b15dfb6fdcdd3a59dc645ace8
You Jeonghan <57481424+youjeonghan@users.noreply.github.com>
2020-11-26 02:39:36 +0900

    post.py 리팩토링 (20.11.25)
    post_like 라우터 함수
    comment_like 라우터 함수
    
    리팩토링

--- TeamProject/api/decoration.py
+++ TeamProject/api/decoration.py
@@ -3,14 +3,14 @@ from flask import current_app
 from flask_jwt_extended import get_jwt_identity, verify_jwt_in_request
 from functools import wraps
 
-def admin_required(func):
-	@wraps(func)
-	def wrapper(*args, **kwargs):
-		verify_jwt_in_request()
-		user_id = get_jwt_identity()
-		if user_id != current_app.config['ADMIN_ID']:
-			return {"msg":"Bad Access Token"}, 201
-		result = func(*args, **kwargs)
-		return result
-	return wrapper
 
+def admin_required(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        verify_jwt_in_request()
+        user_id = get_jwt_identity()
+        if user_id != current_app.config["ADMIN_ID"]:
+            return {"msg": "Bad Access Token"}, 201
+        return func(*args, **kwargs)
+
+    return wrapper
