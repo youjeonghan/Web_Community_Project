1f4b50d1464fb5b0a846cd9e4db4fb15466ec8aa
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2020-11-30 02:15:12 +0900

    중간저장

--- TeamProject/api/admin.py
+++ TeamProject/api/admin.py
@@ -194,75 +194,7 @@ def comment_report_list_delete():
     return jsonify(result="success"), 204
 
 
-# 블랙리스트 정지
-# @api.route("/admin/blacklist", methods=["POST"])
-# @admin_required
-# def blacklist():
-#     data = request.get_json()
-#     userid = data.get("user_id")  # 유저 프라이머리키
-#     post_id = data.get("post_id")
-#     comment_id = data.get("comment_id")
-#     punishment_date = int(data.get("punishment_date"))  # 정지 일수
 
-#     if post_id != "":  # 포스트 프라이머리키가 들어오면 해당 게시글 아이디 정지와 동시에 삭제
-
-#         post = Post.query.filter(Post.id == post_id).first()
-#         board = Board.query.filter(Board.id == post.board_id).first()
-#         board.post_num -= 1
-
-#         # post 삭제하기전 post에 속한 img 먼저 삭제
-#         del_img_list = Post_img.query.filter(Post_img.post_id == id).all()
-#         floder_url = "static/img/post_img/"
-#         for file in del_img_list:
-#             file_url = floder_url + file.filename
-#             if os.path.isfile(file_url):
-#                 os.remove(file_url)
-
-#         db.session.delete(post)
-#         db.session.commit()
-
-#     else:  # 댓글 프라이머리키가 들어오면 해당 댓글 삭제
-#         comment = Comment.query.filter(Comment.id == comment_id).first()
-#         comment.content = "이미 삭제된 댓글입니다."
-#         comment.report_num = 0
-#         db.session.commit()
-
-#     Black_history = Blacklist.query.filter(Blacklist.userid == userid).first()
-#     if Black_history:  # 전에 블랙먹은 기록이 있는가?
-#         if (
-#             Black_history.punishment_date < punishment_date
-#         ):  # 전에 정지 이수와 현재 정지 일수를 비교하여 큰 수로 정지
-#             if punishment_date > 30:  # 영구정지(30일이 넘는 숫자를 입력받으면 영구정지로 처리)
-#                 punishment_end = datetime(4000, 1, 1)
-
-#             else:
-#                 punishment_start = datetime.now()
-#                 punishment_end = punishment_start + timedelta(days=int(punishment_date))
-#         else:  # 전에 먹은 정지 일수로 유지
-#             return jsonify(result="블랙리스트에 추가되었습니다."), 202
-#         Black_history.punishment_date = punishment_date
-#         Black_history.punishment_end = punishment_end
-#         db.session.commit()
-#         return jsonify(result="블랙리스트에 추가되었습니다."), 202
-
-#     else:  # 정지먹은 적이 없으므로
-#         if punishment_date > 30:  # 영구정지(30일이 넘는 숫자를 입력받으면 영구정지로 처리)
-#             punishment_end = datetime(4000, 1, 1)
-#         else:
-#             punishment_start = datetime.now()
-#             punishment_end = punishment_start + timedelta(days=int(punishment_date))
-
-#     user = User.query.filter(User.id == userid).first()  # 프라이머리키로 유저 찾기
-
-#     Black = Blacklist()
-#     Black.userid = user.id
-#     Black.user = user
-#     Black.punishment_date = punishment_date
-#     Black.punishment_end = punishment_end
-
-#     db.session.add(Black)
-#     db.session.commit()
-#     return jsonify(result="블랙리스트에 추가되었습니다."), 202
 
 # 블랙리스트 게시글로 정지
 @api.route("/admin/post-blacklist", methods=["POST"])
@@ -290,15 +222,6 @@ def comment_blacklist():
     #유저 프라이머리키 , 정지일수
     detail_blacklist(data.get("user_id"),int(data.get("punishment_date")))
     return jsonify(result="블랙리스트에 추가되었습니다."), 202
-
-# # 블랙리스트 조회
-# @api.route("/admin/who_is_black")
-# @admin_required
-# def who_is_black():
-#     blacklist = Blacklist.query.order_by(
-#         Blacklist.punishment_end.desc()
-#     ).all()  # 블랙리스트 정지 풀리는 날짜가 느린 순으로 반환
-#     return jsonify([black.serialize for black in blacklist]), 201
 
 
 # 유저 정보 전부 반환
@@ -336,7 +259,7 @@ def user_delete(id):
 @admin_required
 def user_nickname_modify(id):
     nickname = request.form.get("nickname")
-    check_user = User.query.filter(User.id == id).first()
+    check_user = search_table_by_id(User,id)
     updated_data = {}
     if nickname and nickname != check_user.nickname:  # 바꿀 nickname을 입력받으면
         if User.query.filter(User.nickname == nickname).first():  # nickname 중복 검사
--- TeamProject/controllers/user_controller.py
+++ TeamProject/controllers/user_controller.py
@@ -13,17 +13,6 @@ from controllers.temp_controller import *
 from config import *
 
 
-# # 이미지 기본 설정
-# def allowed_file(file):
-#     check = 1
-#     if (
-#         file.filename.rsplit(".", 1)[1].lower() not in ALLOWED_EXTENSIONS
-#         or "." not in file.filename
-#     ):
-#         check = 0
-
-#     return check
-
 
 def pwd_check(password):
     result = {}
@@ -89,18 +78,6 @@ def check_signup(data):
 
     return {}, False
 
-
-# def manufacture_img(input_img, folderurl):
-#     # 사진 이름 테이블에 삽입 및 저장
-#     if input_img and allowed_file(input_img):  #  이미지 확장자 확인
-#         suffix = datetime.now().strftime("%y%m%d_%H%M%S")
-#         filename = "_".join(
-#             [input_img.filename.rsplit(".", 1)[0], suffix]
-#         )  # 중복된 이름의 사진을 받기위해서 파일명에 시간을 붙임
-#         extension = input_img.filename.rsplit(".", 1)[1]
-#         filename = secure_filename(f"{filename}.{extension}")
-#         input_img.save(os.path.join(folderurl, filename))
-#         return filename
 
 
 def store_signup_db(data):
